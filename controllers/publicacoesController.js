2bfdb674e96b9ee8f72d497d817382fda52ef290
Evelyn Correa <evelynfcorrea@gmail.com>
2023-10-11 22:17:16 -0300

    separate posts results by year

--- controllers/publicacoesController.js
+++ controllers/publicacoesController.js
@@ -5,7 +5,19 @@ const getPublications = async (req, res) => {
         const allYears = response.map(post => ( post?.date ));
         const filteredYears = [...new Set(allYears)].sort((a, b) => b - a);
 
-        res.status(200).send({ data: response, years: filteredYears });
+        const result = {};
+
+        response.forEach(entry => {
+            const { date } = entry;
+
+            if (!result[date]) {
+                result[date] = [];
+            }
+            
+            result[date].push(entry);
+        });
+
+        res.status(200).send({ data: response, years: filteredYears, separatedData: result });
     }).catch(error => {
         res.status(404).send(error);
     })
